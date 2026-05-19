702c97adb7287913ba68c12788441b063d935948
Gauravpy-ctrl <23051830@kiit.ac.in>
2026-05-19 10:16:16 +0530

    fixed openrouter final

--- services/openrouter.ts
+++ services/openrouter.ts
@@ -11,10 +11,14 @@ export async function askAI(prompt: string) {
           "Content-Type": "application/json",
 
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENROUTER_API_KEY}`,
+
+          "HTTP-Referer": "https://yourstudybuddy-3f8o.vercel.app",
+
+          "X-Title": "StudyBUDDY",
         },
 
         body: JSON.stringify({
-          model: "openai/gpt-3.5-turbo",
+          model: "meta-llama/llama-3-8b-instruct",
 
           messages: [
             {
@@ -30,48 +34,9 @@ export async function askAI(prompt: string) {
 
     console.log(data);
 
-    if (!data.choices) {
-      return "OpenRouter API Error";
+    if (data.error) {
+      return data.error.message;
     }
-
-    return data.choices[0].message.content;
-
-  } catch (error) {
-
-    console.error(error);
-
-    return "AI is currently unavailable.";
-
-  }
-}export async function askAI(prompt: string) {
-
-  try {
-
-    const response = await fetch(
-      "https://openrouter.ai/api/v1/chat/completions",
-      {
-        method: "POST",
-
-        headers: {
-          "Content-Type": "application/json",
-
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENROUTER_API_KEY}`,
-        },
-
-        body: JSON.stringify({
-          model: "openai/gpt-3.5-turbo",
-
-          messages: [
-            {
-                role: "user",
-                content: prompt,
-            },
-          ],
-        }),
-      }
-    );
-
-    const data = await response.json();
 
     return data.choices[0].message.content;
 
